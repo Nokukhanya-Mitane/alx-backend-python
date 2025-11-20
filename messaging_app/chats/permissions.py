92a4f10660309d300937b6a4f45d9325fe1cd8bc
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2025-11-20 17:30:43 +0200

    Update permissions.py

--- messaging_app/chats/permissions.py
+++ messaging_app/chats/permissions.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 from rest_framework.permissions import BasePermission
+from rest_framework import permissions
 
-class IsParticipant(BasePermission):
+
+class IsParticipant(permissions.BasePermission):
     """
     Allow access only to users who are part of the conversation.
     """
-
     def has_object_permission(self, request, view, obj):
         return request.user in obj.participants.all()
 
 
-class IsMessageSender(BasePermission):
+class IsMessageSender(permissions.BasePermission):
     """
     Allow sending/editing messages only by the sender.
     """
-
     def has_object_permission(self, request, view, obj):
         return obj.sender == request.user
