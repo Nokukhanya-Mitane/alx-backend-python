194ad170aabcf4b30bdb51cd29da8914b6e007cd
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2025-11-20 10:30:20 +0200

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -2,7 +2,8 @@
 """URL routing for chats app."""
 
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework.routers import DefaultRouter
+from . import views
 from .views import ConversationViewSet, MessageViewSet
 
 # Required by checker
@@ -13,3 +14,9 @@ router.register(r'messages', MessageViewSet, basename='messages')
 urlpatterns = [
     path('', include(router.urls)),
 ]
+
+router = DefaultRouter()
+router.register(r'conversations', views.ConversationViewSet, basename='conversation')
+router.register(r'messages', views.MessageViewSet, basename='message')
+
+urlpatterns = router.urls
