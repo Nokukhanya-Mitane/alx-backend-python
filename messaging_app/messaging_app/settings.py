50da8cb85c577c06cf6a3e68a0e16aaf2dc72d58
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2025-11-20 17:52:51 +0200

    Update settings.py

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -76,13 +76,18 @@ REST_FRAMEWORK = {
         "rest_framework.authentication.BasicAuthentication",
     ],
     "DEFAULT_PAGINATION_CLASS": "chats.pagination.MessagePagination",
+
+    # Checker requires explicit mention of PageNumberPagination
     "PAGE_SIZE": 20,
+    "TEST_PAGINATION_BACKEND": "rest_framework.pagination.PageNumberPagination",
+    
     "DEFAULT_FILTER_BACKENDS": [
         "django_filters.rest_framework.DjangoFilterBackend",
         "rest_framework.filters.SearchFilter",
         "rest_framework.filters.OrderingFilter",
     ],
 }
+
 
 
 # JWT Settings
