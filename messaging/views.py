f1c7190d6bff47d7565cda7cdeb844252ba8d55f
Nokukhanya-Mitane <nokukhanyamitane@gmail.com>
2026-01-11 18:43:07 +0200

    Create views.py

---
+++ messaging/views.py
@@ -0,0 +1,9 @@
+from django.views.decorators.cache import cache_page
+from django.shortcuts import render
+from .models import Message
+
+
+@cache_page(60)
+def conversation_messages(request):
+    messages = Message.objects.all()
+    return render(request, 'messages.html', {'messages': messages})
